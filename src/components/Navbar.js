8878d6310ff5f85976abf52122ca5a6bab8c08c2
felix <felixfranko66@gmail.com>
2022-04-11 22:02:21 +0300

    v3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const currentUser = { displayName: "felix franko" };
+  // const currentUser = false;
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
@@ -9,6 +11,21 @@ const Navbar = () => {
           <Link to={"/"} className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
+          {currentUser ? (
+            <div className="d-flex text-white align-items-center">
+              <h5 className="mb-0 text-capitalize">
+                {currentUser?.displayName}
+              </h5>
+            </div>
+          ) : (
+            <button className="ms-2 btn btn-outline-light">Login</button>
+          )}
+
+          {currentUser ? (
+            <button className="ms-2 btn btn-outline-light">Logout</button>
+          ) : (
+            <button className="ms-2 btn btn-outline-light">Register</button>
+          )}
         </div>
       </nav>
     </div>
