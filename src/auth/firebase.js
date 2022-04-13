67ae7d40f440426cf5d366377a3a7ad2c32aeac9
felix <felixfranko66@gmail.com>
2022-04-13 21:33:53 +0300

    v1

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -4,6 +4,7 @@ import {
   getAuth,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
 } from "firebase/auth";
 
 //* https://firebase.google.com/docs/auth/web/start
@@ -30,13 +31,16 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, displayName, navigate) => {
   try {
     let userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
     navigate("/");
     console.log(userCredential);
   } catch (err) {
