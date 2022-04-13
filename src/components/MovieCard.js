fefcb875c33dd54712c62dd2d12b32749aa8d7aa
felix <felixfranko66@gmail.com>
2022-04-13 23:16:49 +0300

    v8

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -32,6 +32,10 @@ const MovieCard = ({ title, poster_path, overview, vote_average, id }) => {
           </span>
         )}
       </div>
+      <div className="movie-over">
+        <h2>Overview</h2>
+        <p>{overview}</p>
+      </div>
     </div>
   );
 };
